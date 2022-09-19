3c4c8d2b0f1a9e83669223dbca9a039ea3e37372
Seemya Mohamed <mohamedsh2@alumni.vcu.edu>
2022-09-19 10:14:05 -0400

    add helloworld.py

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,4 +1,12 @@
-def main():
+def main():  # type: ignore
     print("Hello World!")
+
+main()
+
+def hello_world(city):
+    print(f'Hello World from {city}')
+
+def main():
+    hello_world('NYC')
 
 main()
